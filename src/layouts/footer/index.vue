76c4850d5b9e9ac5a053b5f6db6022e46d0e0b64
高江华 <g598670138@163.com>
2023-12-22 21:36:53 +0800

    docs(初始新项目): 📚 初始化网站项目

--- src/layouts/footer/index.vue
+++ src/layouts/footer/index.vue
@@ -10,50 +10,16 @@
 -->
 <template>
     <div class='page flex flex-col items-center fixed bottom-0 left-0 py-2'>
-        <div class='urls flex'>
-            <a class='btnn box box-cd cursor-pointer' target="_blank" href="https://wolffy.gaojianghua.cn">Wolffy印记</a>
-            <div class='px-2'/>
-            <a class="btnn box box-cd cursor-pointer" target="_blank" href="https://docs.gaojianghua.cn">作者文档</a>
-            <div class='px-2'/>
-            <a class="btnn box box-cd cursor-pointer" target="_blank" href="https://gaojianghua.cn">服务官网</a>
-        </div>
-        <div class='text cursor-pointer mt-3'>邮箱：g598670138@163.com 个人微信号：woshigaojianghua</div>
-        <a target="_blank" rel="noreferrer" href="https://beian.miit.gov.cn" class='text cursor-pointer mt-3'>Copyright ©
-            2022-{{copyright}} JiangHua Gao 版权所有 浙ICP备2022001576号</a>
+        
     </div>
 </template>
 
 <script setup lang='ts'>
-import { ref } from 'vue'
 
-let copyright = ref(new Date().getFullYear())
 </script>
 
 <style lang='scss' scoped>
 .page {
-    width: 100%;
-    height: 10rem;
 
-    .urls {
-        padding-bottom: .9rem;
-
-        .btnn {
-            padding: .33rem 1.2rem;
-        }
-
-        .btnn:hover {
-            background-color: rgba(255, 255, 255);
-            color: var(--main-color) !important;
-        }
-    }
-
-    .text {
-        color: #969faf;
-        font-size: 14px;
-    }
-
-    .text:hover {
-        color:var(--main-color);
-    }
 }
 </style>
